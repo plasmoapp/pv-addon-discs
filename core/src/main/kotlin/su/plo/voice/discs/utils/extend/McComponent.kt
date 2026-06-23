c98180b94844750760d22a9ac855e165eeb4bd43
Apehum <apehum@proton.me>
2026-06-23 13:25:17 +0800

    fix: replace PlainComponentSerializer with PlainTextComponentSerializer

--- core/src/main/kotlin/su/plo/voice/discs/utils/extend/McComponent.kt
+++ core/src/main/kotlin/su/plo/voice/discs/utils/extend/McComponent.kt
@@ -2,13 +2,13 @@ package su.plo.voice.discs.utils.extend
 
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer
-import net.kyori.adventure.text.serializer.plain.PlainComponentSerializer
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import su.plo.slib.api.chat.component.McTranslatableText
 import su.plo.voice.api.server.PlasmoVoiceServer
 import su.plo.voice.api.server.player.VoicePlayer
 
 fun Component.toPlainText(): String =
-    PlainComponentSerializer.plain().serialize(this)
+    PlainTextComponentSerializer.plainText().serialize(this)
 
 fun McTranslatableText.render(voicePlayer: VoicePlayer, voiceServer: PlasmoVoiceServer): Component {
     val json = voiceServer.minecraftServer
